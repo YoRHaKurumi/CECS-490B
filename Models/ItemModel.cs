9fe0aba5632d80cca75fe85a3779fb8de425a040
YoRHaKurumi <114028069+YoRHaKurumi@users.noreply.github.com>
2023-05-10 17:41:33 -0700

    Add files via upload

---
+++ Models/ItemModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MauiApp1.Models
+{
+    
+    public class ItemModel
+    {
+        
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public string Location { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
