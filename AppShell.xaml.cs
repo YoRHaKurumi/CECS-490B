e9b07e99be796190dffeff0fd63ccd2aa044fb06
YoRHaKurumi <114028069+YoRHaKurumi@users.noreply.github.com>
2023-05-10 17:44:45 -0700

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiApp1.Views;
+
+namespace MauiApp1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(ItemDetailView), typeof(ItemDetailView));
+        Routing.RegisterRoute(nameof(AddUpdateUserItemDetail), typeof(AddUpdateUserItemDetail));
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using MauiApp1.Services;
+using MauiApp1.ViewModels;
+using MauiApp1.Views;
+
+
+namespace MauiApp1;
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+        
+        //Services
+        builder.Services.AddSingleton<IItemService, ItemService>();
+
+		//Views Registration
+		builder.Services.AddSingleton<UserItemListPage>();
+        builder.Services.AddSingleton<AddUpdateUserItemDetail>();
+
+        //View Models
+        builder.Services.AddSingleton<UserListPage>();
+        builder.Services.AddSingleton<AddUpdateUserItem>();
+
+        return builder.Build();
+	}
+}
